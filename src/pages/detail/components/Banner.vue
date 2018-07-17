976b864d67cc8879d3f42c22a604aee087e4a1ea
zhanwei <375786117@qq.com>
2018-07-17 21:38:11 +0800

    'banner'

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="banner">
         <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1611/78/786b5749da2dceb5a3.water.jpg_r_800x800_b52c7865.jpg"/>
-        <div >常州中华恐龙园(AAAAA景区)</div>
-        <div ></div>
-    </div>
+        <div class="banner-info">
+          <div class="banner-title">常州中华恐龙园(AAAAA景区)</div>
+          <div class="banner-number">
+            <span class="iconfont">&#xe608;</span>
+            39
+          </div>
+        </div>
+    </div> 
 </template>
 
 <script>
@@ -15,9 +20,29 @@ export default {
 
 <style lang="stylus" scoped>
   .banner
+    position relative
     overflow: hidden;
     height 0
     padding-bottom 55%
     .banner-img
       width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #fff
+      background-image linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8))//info层颜色渐变
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        padding 0 .4rem
+        height .4rem
+        line-height .4rem
+        border-radius .2rem
+        background rgba(0,0,0,.8)
 </style>
